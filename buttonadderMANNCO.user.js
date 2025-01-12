af5fbd869abc0153d3e8641fb39d1e75d894810d
yaboieeek <typarasa@gmail.com>
2025-01-12 16:51:24 +0300

    added bptf history checker for all items

--- buttonadderMANNCO.user.js
+++ buttonadderMANNCO.user.js
@@ -4,8 +4,8 @@
 // @version      2025-01-11
 // @description  it haks ur compure
 // @author       eeek
-// @match       https://mannco.store/item/440-*-unusual-*
-// @match       https://mannco.store/ru/item/440-*-unusual-*
+// @match       https://mannco.store/item/440-*
+// @match       https://mannco.store/ru/item/440-*
 // @require http://code.jquery.com/jquery-latest.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=stntrading.eu
 // @grant        none
@@ -14,10 +14,25 @@
 function getItem() {
     return $('.item-info__name').text().replace('★', '').replace('Unusual', '').replace(/\s+/g, " ").trim();
 };
+function checker() {return $('.item-info__name').text().includes('★')};
 
-let $bpBtn = $('<button>', {class: 'btn btn-sm justify-content-center'});
+if (checker()) {
+    let $bpBtn = $('<button>', {class: 'btn btn-sm justify-content-center'});
 
-$('.item-info__data').append($bpBtn);
+    $('.item-info__data').append($bpBtn);
+    $bpBtn.css({'font-size': '2rem', 'background-color': '#189ab9', 'border-color': '#35bddb'}).html('BPtf stats');
+    $bpBtn.on('click', function() {
+        fetch(`https://backpack.tf/search?text=${getItem()}`).then(result => {return result.json()})
+            .then(data => {let res = data.results[0];
+                           if (!res){
+                               alert(`Can't find ${getItem()}. If the item name looks fine, the problem is on bptf side!`)
+                           }else{
+                               window.open(`https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`)
+                           }
+                          })
+
+    })
+}
 
 let $bpHistory = $('.table-items__actions > a').each(function () {
     if($(this).text().includes('Steamcollector.com')) {
@@ -25,20 +40,4 @@ let $bpHistory = $('.table-items__actions > a').each(function () {
         $(this).attr('href', `https://backpack.tf/item/${$item}`)
             .text('View history on backpack.tf');
     }
-})
-;
-
-
-
-$bpBtn.css({'font-size': '2rem', 'background-color': '#189ab9', 'border-color': '#35bddb'}).html('BPtf stats');
-$bpBtn.on('click', function() {
-    fetch(`https://backpack.tf/search?text=${getItem()}`).then(result => {return result.json()})
-        .then(data => {let res = data.results[0];
-              if (!res){
-                	alert(`Can't find ${getItem()}. If the item name looks fine, the problem is on bptf side!`)
-                }else{
-                	window.open(`https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`)
-                }
-    })
-
-})
+});
