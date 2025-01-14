10134093a2782691b8c0a96d349f9de12f345ccd
yaboieeek <typarasa@gmail.com>
2025-01-14 19:14:16 +0300

    small bug fixes

--- buttonadderultimate.user.js
+++ buttonadderultimate.user.js
@@ -7,19 +7,18 @@
 // @match        https://stntrading.eu/item/tf2/Unusual+*
 // @require http://code.jquery.com/jquery-latest.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=stntrading.eu
+// @downloadURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/buttonadderultimate.user.js
 // @grant        none
 // ==/UserScript==
 
 function getItem() {
-    return $('.card-title').html()
+    let i = $('.card-title').html();
+    return i;
 };
 
 let $bpBtn = $('<button>', {class: 'btn xx'});
-
 $('.justify-content-sm-start').append($bpBtn);
 
-let $itemInfo = $('.card-title').text();
-console.log($itemInfo);
 
 $bpBtn.css({width: '25%', 'margin-left': '3%', border: 'none','border-radius': 0, 'padding-top': '0', 'background-color': '#cc77cc', color: 'white'}).html('BPtf stats');
     $bpBtn.on('click', function() {
@@ -27,7 +26,7 @@ $bpBtn.css({width: '25%', 'margin-left': '3%', border: 'none','border-radius': 0
             .then(data => {let res = data.results[0];
                            if (!res){
                                console.info('BPTF-API-ERROR: no item found. Redirecting...');
-                               window.open(`https://google.com/search?q=${$itemInfo.replace(/\s+/g, ' ')}`);
+                               window.open(`https://google.com/search?q=${getItem()}`);
                            }else{
                                window.open(`https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`)
                                console.log(res.values);
