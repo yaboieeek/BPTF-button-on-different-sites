c0cdc0dfffc91a966459a0d9ce0a9e50bcd2192c
yaboieeek <typarasa@gmail.com>
2025-01-18 01:30:32 +0300

    Update MCSRbutton.user.js

--- MCSRbutton.user.js
+++ MCSRbutton.user.js
@@ -13,15 +13,16 @@
 
 let $bList = $('.price-boxes');
 let $itemName = $('.stats-header-item>div').attr('data-name').trim().toLowerCase();
+let itemName = $itemName.replace(/'/g, '');
 let $itemEffect = $('.stats-header-title').text().toLowerCase().replace($itemName, '').trim().replace($itemName, '');
 
-let $mcBtn = $('<a>', {class: 'price-box', href: `https://mannco.store/item/440-${$itemEffect.replace(/\s/g, '-')}-unusual-${$itemName.replace(/\s/g, '-')}`, target: '_blank'}).css('width', '11em');
+let $mcBtn = $('<a>', {class: 'price-box', href: `https://mannco.store/item/440-${$itemEffect.replace(/\s/g, '-')}-unusual-${itemName.replace(/\s/g, '-')}`, target: '_blank'}).css('width', '11em');
 let insides = $('<div>', {class: 'value'}).text('View on').css({'white-space': 'nowrap'});
 let ico = $('<div>', {class: 'icon'}).append($('<img>', {class: 'stm', src: 'https://i.ibb.co/YDPhFRS/images.png'}).css({width: '40px', height: '38px', 'margin-right': 0 }).css('margin-top', '-5px'));
 let $text = $('<div>').text('MannCo Store').css({'font-weight': 'bold', position: 'relative', top: '1em', left: '-3.5em', 'white-space': 'nowrap'});
 $bList.append($mcBtn);
 $mcBtn.append(ico, insides, $text);
-clearInterval(refresh);
+
 
 
 
