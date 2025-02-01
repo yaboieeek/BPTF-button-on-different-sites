7ba14dc219cc54ab1df6b67c634dd71f4f3263f3
yaboieeek <typarasa@gmail.com>
2025-02-01 14:17:44 +0300

    small bug fixes

--- AdvancedLinksOnBPTF.user.js
+++ AdvancedLinksOnBPTF.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BPTF advanced links
 // @namespace    amogus
-// @version      0.2
+// @version      0.3
 // @description  mannCo and stn button on bptf
 // @author       eeek
 // @match        https://backpack.tf/stats/Unusual/*/Tradable/*
@@ -17,7 +17,7 @@ let itemName = $itemName.replace(/'/g, '');
 let $itemEffect = $('.stats-header-title').text().toLowerCase().replace($itemName, '').trim().replace($itemName, '');
 
 
-let $mcBtn = $('<a>', {class: 'price-box', href: `https://mannco.store/item/440-${$itemEffect.replace(/\s/g, '-')}-unusual-${itemName.replace(/\s/g, '-')}`, target: '_blank'}).css('width', '11em');
+let $mcBtn = $('<a>', {class: 'price-box', href: `https://mannco.store/item/440-${$itemEffect.replace(/\s/g, '-').replace(/:/g, '')}-unusual-${itemName.replace(/\s/g, '-').replace(/:/g, '')}`, target: '_blank'}).css('width', '11em');
 let insides = $('<div>', {class: 'value'}).text('View on').css({'white-space': 'nowrap'});
 let ico = $('<div>', {class: 'icon'}).append($('<img>', {class: 'stm', src: 'https://i.ibb.co/YDPhFRS/images.png'}).css({width: '40px', height: '38px', 'margin-right': 0 }).css('margin-top', '-5px'));
 let $text = $('<div>').text('MannCo Store').css({'font-weight': 'bold', position: 'relative', top: '1em', left: '-3.5em', 'white-space': 'nowrap'});
