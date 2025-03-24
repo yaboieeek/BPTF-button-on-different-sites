043f7a25ce1fdf65703037388c879d44e508a789
yaboieeek <typarasa@gmail.com>
2025-03-24 11:48:59 +0300

    hotfix after some cors update

--- buttonadderultimate.user.js
+++ buttonadderultimate.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BPTF button on stn!
-// @version      1.4
+// @version      1.5
 // @description  it haks ur compure
 // @author       eeek
 // @match        https://stntrading.eu/item/tf2/Unusual+*
@@ -10,9 +10,10 @@
 // @updateURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/buttonadderultimate.user.js
 // @grant GM_setValue
 // @grant GM_getValue
+// @grant GM_xmlhttpRequest
+
 // ==/UserScript==
 
-window.onload = function () {
 function getItem() {
     let i = $('.card-title').html();
     return i;
@@ -51,6 +52,14 @@ let heart = $wBtn.children();
 let search = $rBtn.children()
 $wBtn.empty().append(heart)
 $rBtn.empty().append(search)
+
+
+/////////////////////////////// buwomp
+let bUrl = `https://backpack.tf/api/classifieds/listings/snapshot?sku=${getItem().replace('Unusual', '').trim()}&appid=440&token=${GM_getValue('userToken')}`
+
+////////////////////////////////////////////////////////
+
+
 //functions, some unnecessary things are here but lolol im coder not you
 $rqSetting.on('click', requester);
 let done = false;
@@ -61,80 +70,86 @@ $rqPrice.on('click', function (){
         $rqWindow.toggle();
         if (!done) {
             done = true
-
             let cBuy = 0; let cSell = 0;
             //it generates link using item's title on stn
-            let bUrl = `https://backpack.tf/api/classifieds/listings/snapshot?sku=${getItem().replace('Unusual', '').trim()}&appid=440&token=${GM_getValue('userToken')}`;
-
-            fetch(bUrl).then(r => r.json()).then(d => {
-                //error handler
-                if (!d.listings && d.message) {
-                    console.info("There was an error getting listings. Check your token. If it's correct, the problem is on bptf's side");
-                } else if (!d.listings && !d.message){ console.log("BPTF API error! This item's price can not be fetched")} else {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: bUrl,
+                onload: function(response) {
+                    const d = JSON.parse(response.responseText);
+                    if (!d.listings && d.message) {
+                        console.info("There was an error getting listings. Check your token. If it's correct, the problem is on bptf's side");
+                    } else if (!d.listings && !d.message) {
+                        console.log("BPTF API error! This item's price can not be fetched")
+                    } else {
                         d.listings.forEach(el => {
                             if (el.intent == 'sell') {cSell++};
                         });
-                //////////////
-
-                        let sellStart = d.listings
-                        .find(e =>
+                        let sellStart = d.listings.find(e =>
                               e.intent == 'sell');
                         if (!sellStart) {
                             $sell.text('No listings!');
-                        } else {$sell.text(`Buy: ${sellStart.currencies.keys? sellStart.currencies.keys + ' keys': '$' + sellStart.currencies.usd} ${sellStart.currencies.metal ? `${sellStart.currencies.metal} ref` : ''}`);}
-                        let buyStart = d.listings
-                        .find(el =>
+                        } else {
+                            $sell.text(`Buy: ${sellStart.currencies.keys? sellStart.currencies.keys + ' keys': '$' + sellStart.currencies.usd} ${sellStart.currencies.metal ? `${sellStart.currencies.metal} ref` : ''}`);}
+                        let buyStart = d.listings.find(el =>
                               el.intent == 'buy' && !(el.item.attributes.some(cc => /\b1[0-9]{3}\b/.test(cc.defindex))));
-
                         let $bStn = $('.text-center p.mb-0:nth-of-type(1) + p.mb-0').text();
-
                         const regex = /(\d+)\s+keys(,\s+([\d.]+)\s+ref)?/;
                         const match = $bStn.match(regex);
                         let keys, ref;
 
                         if (match) {
-                            keys = match[1]; 
+                            keys = match[1];
                             ref = match[3];
                         };
 
                         $rqWindow.css('display', 'none')? $rqWindow.css('display', 'block'): $rqWindow.css('display', 'none');
                         $sellCount.text('Sellers: ' + cSell);
                         $buy.text(`Sell: ${buyStart.currencies.keys} keys ${buyStart.currencies.metal ? `${buyStart.currencies.metal} ref` : ''}`);
-                        switch (cSell){
+                        switch (cSell) {
                             case 0:
                             case 1: $sellCount.css('color', '#5D5'); break;
-                            case 2: case 3: $sellCount.css('color', '#DD3'); break;
+                            case 2: $sellCount.css('color', '#DD3'); break;
                             default: $sellCount.css('color', '#E55'); break;
                         }
 
-                    // keys/ref is STN buy price, //buyStart.currencies is buy orders
-                    // this one is checking what price is the best including refs (hopefully you wont find items that have 0.*ref difference, lol)
-                    if (keys > buyStart.currencies.keys || (keys == buyStart.currencies.keys && ref > buyStart.currencies.metal)) {
-                        console.log (buyStart.currencies.ref || 400, ref|| 400);
-                        $('#buy').css('color', '#D55'); $('.text-center p.mb-0:nth-of-type(1) + p.mb-0').first().css('color', '#9f9')
-                    } else {
-                        console.log(buyStart.currencies.ref || 400, ref|| 400)
-                        $('#buy').css('color', '#9f9'); $('.text-center p.mb-0:nth-of-type(1) + p.mb-0').first().css('color', '#D55')
-                    }
+                        // keys/ref is STN buy price, //buyStart.currencies is buy orders
+                        // this one is checking what price is the best including refs (hopefully you wont find items that have 0.*ref difference, lol)
+                        if (keys > buyStart.currencies.keys || (keys == buyStart.currencies.keys && ref > buyStart.currencies.metal)) {
+                            console.log (buyStart.currencies.ref || 400, ref|| 400);
+                            $('#buy').css('color', '#D55'); $('.text-center p.mb-0:nth-of-type(1) + p.mb-0').first().css('color', '#9f9')
+                        } else {
+                            console.log(buyStart.currencies.ref || 400, ref|| 400)
+                            $('#buy').css('color', '#9f9'); $('.text-center p.mb-0:nth-of-type(1) + p.mb-0').first().css('color', '#D55')
+                        }
 
                     }
-        });} else {console.log('dont spam this button pls')}
-    }
+    },
+                onerror: function(error) {
+                    console.error('FETCH ERROR: ', error);
+                }
+            }) } else {
+            console.log('Click click click')
+            }
+    };
 })
 
 //It checks, if item can be found through bptf search api; if it fails, you will be redirected to google which is fiiiiine i guess
     $bpBtn.on('click', function() {
-        fetch(`https://backpack.tf/search?text=${getItem()}`).then(result => {return result.json()})
-            .then(data => {let res = data.results[0];
-                           if (!res){
-                               console.info('BPTF-SEARCH-API-ERROR: no item found. Redirecting to google...');
-                               window.open(`https://google.com/search?q=${getItem()}`);
-                           }else{
-                               let u = `https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`;
-                               console.info('BPTF-SEARCH-API-SUCCESS: item was found! Redirecting to backpack.tf')
-                               window.open(u)
-                           }
-                          })
-
+        let itemLink = `https://backpack.tf/search?text=${getItem()}`;
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: itemLink,
+            onload: function (response) {
+                let res = JSON.parse(response.responseText).results[0];
+                if (!res){
+                    console.info('BPTF-SEARCH-API-ERROR: no item found. Redirecting to google...');
+                    window.open(`https://google.com/search?q=${getItem()}`);
+                }else{
+                    let u = `https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`;
+                    console.info('BPTF-SEARCH-API-SUCCESS: item was found! Redirecting to backpack.tf')
+                    window.open(u)
+                }
+            }
+        })
     })
-}
