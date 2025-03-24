201eeccb9bf4c06e487872ea096b44c1fe40e7e0
yaboieeek <typarasa@gmail.com>
2025-03-24 18:30:26 +0300

    hotfix

--- buttonadderultimate.user.js
+++ buttonadderultimate.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BPTF button on stn!
-// @version      1.6
+// @version      1.7
 // @description  it haks ur compure
 // @author       eeek
 // @match        https://stntrading.eu/item/tf2/Unusual+*
