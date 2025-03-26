a4b8316b0aa8f3fd2badc1bc4a3cd6241fddcc61
yaboieeek <typarasa@gmail.com>
2025-03-26 13:31:03 +0300

    Replaced http in require with https

--- buttonadderultimate.user.js
+++ buttonadderultimate.user.js
@@ -4,7 +4,7 @@
 // @description  it haks ur compure
 // @author       eeek
 // @match        https://stntrading.eu/item/tf2/Unusual+*
-// @require http://code.jquery.com/jquery-latest.js
+// @require https://code.jquery.com/jquery-latest.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=stntrading.eu
 // @downloadURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/buttonadderultimate.user.js
 // @updateURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/buttonadderultimate.user.js
