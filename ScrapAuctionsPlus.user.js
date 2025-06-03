d060e31ada99c9002adcb4ce493e58033a38f0a7
yaboieeek <typarasa@gmail.com>
2025-06-03 18:03:44 +0300

    fix: fixed being unable to bid for some auction

--- ScrapAuctionsPlus.user.js
+++ ScrapAuctionsPlus.user.js
@@ -1,53 +1,117 @@
 // ==UserScript==
 // @name         ScrapAuction+
 // @namespace    skibidi toilet 2025
-// @version      0.2
+// @version      1.0.0
 // @description  it adds cool buttons
 // @author       eeek
 // @match        https://scrap.tf/auctions*
-// @require http://code.jquery.com/jquery-latest.js
 // @updateURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/ScrapAuctionsPlus.user.js
 // @downloadURL https://github.com/yaboieeek/BPTF-button-on-different-sites/raw/refs/heads/main/ScrapAuctionsPlus.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=scrap.tf
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 
-let $list = $("#main-container .items-container > div");
-$list.each(function () {
-    $(this).append($('<button>', {class: 'history-button'}).append($('<i>', {class: 'fa fa-calendar'}))
-        .css({position: 'relative', right: -$(this).width()*0.7, top: 0, 'white-space': 'nowrap', color:'white', 'background-color': 'rgba(80, 80,80, 0.5)', width: '2rem', 'z-index': 5, 'aspect-ratio': '100%', 'line-height': '1em', border: 'none'})
-                  .on('click', function(event) {
-        const dataId = $(event.currentTarget).closest('div').attr('data-id').trim();
-        window.open(`https://backpack.tf/item/${dataId}`);
-        event.stopPropagation();
-    }));
-        
+(function () {
+    const $itemElements = document.querySelectorAll('.quality5');
 
-    $(this).append($('<button>', {class: 'stat-button'}).append($('<i>', {class: 'stm stm-backpack-tf'}))
-        .css({position: 'relative', top: '2em',right: -$(this).width()*0.42, 'white-space': 'nowrap', color:'white', 'background-color': 'rgba(80, 80,80, 0.5)', width:'2rem', 'z-index': 5, 'aspect-ratio': '100%', 'line-height': '1em', border: 'none'})
-                   .on('click', function(event) {
-        event.stopPropagation();
-        console.log();
-        const dataContent = $(`div[data-id="${$(this).closest('div').attr('data-id')}"]`).data('content');
+    for (const itemElement of $itemElements) {
+        if (document.querySelector('.auction-inventory:has(.quality5)')) return;
+        const $historyButton = document.createElement('button');
+        const $statButton = document.createElement('button');
+        [$historyButton, $statButton].forEach(elem => elem.classList.add('custom-button'))
+        const $buttonsContainer = document.createElement('div');
+        $buttonsContainer.classList.add('bptf-buttons');
+        const historyIcon = document.createElement('i');
+        const statIcon = document.createElement('i');
+        historyIcon.classList.add('fa', 'fa-calendar');
+        statIcon.classList.add('stm', 'stm-backpack-tf');
 
-        const effectMatch = dataContent.match(/Effect:\s*(.*?)(<|$)/);
-        const effect = effectMatch ? effectMatch[1].trim() : null;
+        $buttonsContainer.append($historyButton, $statButton);
+        $historyButton.append(historyIcon);
+        $statButton.append(statIcon);
+        itemElement.append($buttonsContainer);
 
-        const itemName = $('<div>').html($(this).closest('div').attr('data-title')).css({display: 'none'}).text();
-        fetch(`https://backpack.tf/search?text=${effect.trim()} ${itemName.trim()}`).then(result => {return result.json()})
-            .then(data => {let res = data.results[0];
-                           if (!res){
-                               console.info('BPTF-API_ERROR: no item found. Redirecting to google...');
-                               window.open(`https://google.com/search?q=${effect || ''} ${itemName}`);
-                           }else{
-                               let u = `https://backpack.tf/stats/Unusual/${res.item_name}/Tradable/Craftable/${res.values[0].priceindex}`;
-                               console.info('BPTF-API_SUCCESS: item was found! Redirecting to backpack.tf')
-                               window.open(u)
-                           }
-                          })
+        itemElement.style.position = 'relative';
+        const {title: rawTitle, id, content: rawContent} = itemElement.dataset;
 
-    })
-    );
+        const title = parser(rawTitle).replace('★Unusual ', '').trim() || '';
+        const effect = parser(rawContent).replace('★Unusual ', '').trim() || '';
 
-})
+        $historyButton.addEventListener('click', (event) => {
+            event.stopPropagation();
+            window.open(`https://backpack.tf/item/${id}`);
+        });
+        $statButton.addEventListener('click', function (event) {
+            event.stopPropagation();
+            const finalString = encodeURIComponent(`${effect} ${title}`);
+            GM_xmlhttpRequest({
+                url: `https://backpack.tf/search?text=${finalString}`,
+                onload: async function(response){
+                    if (response.status !== 200 || typeof JSON.parse(response.responseText).results[0] === 'undefined') {
+                        ScrapTF.Crouton.Add(`[SrapAuction+] Couldn't find ${effect} ${title}, redirecting to Google`, 'alert-error');
+                        await new Promise(resolve => setTimeout(resolve, 500));
+                        window.open(`https://www.google.com/search?q=${effect + ' ' + title}`);
+                        return;
+                    };
+                    const res = JSON.parse(response.responseText).results;
+                    console.log(res);
+                        ScrapTF.Crouton.Add('[SrapAuction+] Success! Opening the stat page');
+                        await new Promise(resolve => setTimeout(resolve, 500));
+                        const effectIndex = res[0].values[0].priceindex;
+                    window.open(`https://backpack.tf/stats/Unusual/${title}/Tradable/Craftable/${effectIndex}`)
+                }
+            })
+        })
+    };
+    function parser(elementHTML) {
+        const parseElement = document.createElement('div');
+        parseElement.innerHTML = elementHTML;
+
+        switch (parseElement.children.length) {
+            case 1: {
+                const innerText = parseElement.innerText;
+                parseElement.remove();
+                return innerText;
+            }
+            case 0: return 'undefined name';
+            default: {
+                try {
+                    const innerText = parseElement.innerHTML.toString().match(/<br>Effect: (.*?)<br>/)[1];
+                    console.log(innerText);
+                    parseElement.remove();
+                    return innerText;
+                } catch(error) {
+                    return '';
+                }
+            }
+        }
+
+    }
+
+    const CUSTOM_BUTTONS_STYLE = document.createElement('style');
+    CUSTOM_BUTTONS_STYLE.innerHTML = `
+          .bptf-buttons {
+              position: absolute;
+              right: 0; top: 0;
+
+              display: flex;
+              flex-direction: column;
+              gap: 5px;
+
+              align-items: center;
+              justify-content: center;
+              line-height: 1;
+
+              button {
+                background-color: rgba(255,255,255,0.2);
+                padding: 5px;
+                aspect-ratio: 1;
+                color: white;
+                border: none
+                }
+          }
+          `
+
+    document.head.append(CUSTOM_BUTTONS_STYLE)
+})();
