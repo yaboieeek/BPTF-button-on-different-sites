f18b53cf527f6200dd7d4aa46549b20a76238ecb
yaboieeek <typarasa@gmail.com>
2025-12-19 12:32:25 +0300

    Create buttonadder.ultimate.js

---
+++ buttonadder.ultimate.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         bptf button on stn!
+// @namespace    https://steamcommunity.com/profiles/76561198967088046
+// @version      7.7.7
+// @description  MIGRATEEEEEE
+// @author       eeek
+// @match        https://scrap.tf/auctions*
+// ==/UserScript==
+
+/// SCRIPT URLS ARE OUTDATED!
+/// SCRIPT REPOSITORY/URLS/EVERYTHING MIGRATED 
+/// REINSTALL IT FROM HERE TO STAY UPDATED: 
+/// https://github.com/yaboieeek/.tf-plus
+
+alert('Please reinstall the script from https://github.com/yaboieeek/.tf-plus');
